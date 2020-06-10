0d2121a8fd54afc1cc2344c96fa36c572a6fcea7
moha-boukhatem <moha94orly@gmail.com>
2020-06-11 00:13:31 +0100

    rien

---
+++ interfaceSecondaire/afficherDemandeTravailPourChef.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <?php include ("php/head.php")?>
+    <title>ChefSection</title>
+</head>
+<body>
+    <?php include ("php/header.php")?>
+    <section class="back col">
+        <div class="row">
+            <a href="ajouterOrdreTravail.php">O.T</a>
+        </div>
+    </section>
+    
+
+    <?php include ("php/footer.php")?>
+
+</body>
+</html>
